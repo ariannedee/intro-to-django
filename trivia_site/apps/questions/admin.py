525c152781a7b3d6cf2c9bd66824090e9492d5f3
ariannedee <ariannedee@gmail.com>
2021-07-19 22:31:09 -0700

    Customize Question list display to show choices

--- trivia_site/apps/questions/admin.py
+++ trivia_site/apps/questions/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Question, Choice
 
@@ -11,3 +12,16 @@ class ChoiceInline(admin.TabularInline):
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
+    list_display = ('pk', 'text', 'choice_list')
+    list_display_links = ('text',)
+
+    def choice_list(self, obj):
+        choices = []
+        for choice in obj.choices.all():
+            if choice.is_correct:
+                choices.append(f"<strong>{choice.text}</strong>")
+            else:
+                choices.append(choice.text)
+        return format_html('<br>'.join(choices))
+
+    choice_list.short_description = "Choices"
