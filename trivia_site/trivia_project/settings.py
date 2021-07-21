f2f63a17d3afb97ef8b6a1de1809e67bad60f291
ariannedee <ariannedee@gmail.com>
2021-07-21 14:57:52 -0700

    Support media files

--- trivia_site/trivia_project/settings.py
+++ trivia_site/trivia_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^)hua^o3^j(0r9j89$k_^p*y77n%hlq6x(r(2k3@mb+4)sdhqm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -132,6 +132,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Media files (user uploaded content)
+# https://docs.djangoproject.com/en/3.2/ref/settings/#std:setting-MEDIA_ROOT
+# https://docs.djangoproject.com/en/3.2/topics/files/
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- trivia_site/trivia_project/urls.py
+++ trivia_site/trivia_project/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include("apps.users.urls", namespace="users")),
+    path('users/', include("apps.users.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='index.html'), name="index"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # https://docs.djangoproject.com/en/3.2/howto/static-files/#serving-uploaded-files-in-development
