3b83f71775fa1954842d070266a24aa6fc86d8ed
ariannedee <ariannedee@gmail.com>
2021-07-23 22:34:17 -0700

    Add a user list view

--- trivia_site/apps/users/urls.py
+++ trivia_site/apps/users/urls.py
@@ -5,13 +5,15 @@ from .views import (
     UserRedirectView,
     UserSignUp,
     UserUpdateView,
+    ListView,
 )
 
 app_name = "users"
 urlpatterns = [
     path("signup/", view=UserSignUp.as_view(), name="signup"),
-    path("~redirect/", view=UserRedirectView.as_view(), name="redirect"),
-    path("~update/", view=UserUpdateView.as_view(), name="update"),
+    path("redirect/", view=UserRedirectView.as_view(), name="redirect"),
+    path("update/", view=UserUpdateView.as_view(), name="update"),
     path("<str:username>/", view=UserDetailView.as_view(), name="detail"),
     path("me", view=UserDetailView.as_view(), name="my-profile"),
+    path("", view=ListView.as_view(), name="list"),
 ]
--- trivia_site/apps/users/views.py
+++ trivia_site/apps/users/views.py
@@ -1,7 +1,11 @@
 from django.contrib import messages
 from django.contrib.auth import get_user_model, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.views import View
+from django.views.decorators.http import require_GET
 from django.views.generic import DetailView, RedirectView, UpdateView, CreateView
 
 from .forms import UserCreationForm, UserChangeForm
@@ -52,3 +56,24 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
 
     def get_redirect_url(self):
         return reverse("index")
+
+
+@require_GET
+@login_required
+def list_view(request):
+    if request.user.is_staff:
+        users = User.objects.all()
+    else:
+        users = User.objects.filter(is_staff=False)
+    context = {"users": users}
+    return render(request, "users/user_list.html", context=context)
+
+
+class ListView(LoginRequiredMixin, View):  # Same functionality as list_view()
+    def get(self, request):
+        if request.user.is_staff:
+            users = User.objects.all()
+        else:
+            users = User.objects.filter(is_staff=False)
+        context = {"users": users}
+        return render(request, "users/user_list.html", context=context)
