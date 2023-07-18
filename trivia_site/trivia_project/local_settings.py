5669b5f300b05d47dbe5282c3681d7705c02f024
ariannedee <ariannedee@gmail.com>
2023-07-18 16:15:37 -0700

    Add local_settings to project

---
+++ trivia_site/trivia_project/local_settings.py
@@ -0,0 +1,11 @@
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
