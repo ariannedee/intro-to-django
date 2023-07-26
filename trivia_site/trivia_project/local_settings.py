64e1d55ef8342035e620ae1dec030b585930e04e
ariannedee <ariannedee@gmail.com>
2023-07-25 23:18:40 -0700

    Maybe email will work

--- trivia_site/trivia_project/local_settings.py
+++ trivia_site/trivia_project/local_settings.py
@@ -9,3 +9,5 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- trivia_site/trivia_project/settings.py
+++ trivia_site/trivia_project/settings.py
@@ -160,6 +160,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 FIXTURE_DIRS = [BASE_DIR / 'fixtures']
 
+# Email for user registration and password reset
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL_ADDRESS", "")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD", "")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
