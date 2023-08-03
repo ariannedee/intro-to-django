0e45888431f0941cb0f882f7652c25158e5ee9ec
ariannedee <ariannedee@gmail.com>
2023-08-02 21:39:22 -0700

    Add django-debug-toolbar to local config

--- trivia_site/trivia_project/settings/local.py
+++ trivia_site/trivia_project/settings/local.py
@@ -12,3 +12,17 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Needed for Django Debug Toolbar
+# https://django-debug-toolbar.readthedocs.io/en/latest/
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
